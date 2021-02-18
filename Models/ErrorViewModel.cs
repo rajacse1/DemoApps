bbcb09f48ec1849e9cdff015648064a84466ff99
Kanimozhi <kanimozhi@purimtechnologies.com>
2021-02-18 12:11:35 +0530

    Changes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreDemoApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
